ed63077802de94c514222713cefa8cddef4a49ef
ufo1990 <dybiec1990@gmail.com>
2023-09-25 12:18:02 +0200

    Role Based Access Control to system

--- PHP/Rbac/Role-based-access-control.php
+++ PHP/Rbac/Role-based-access-control.php
@@ -2,23 +2,14 @@
 /*
 Role Based Access Control to system
 
-//permission
-0 0 0 0 0 0 0 1 - Access system 1
-
-0 0 0 0 0 0 1 0 - Write logs 2 
-0 0 0 0 0 1 0 0 - Read logs  4
-
-0 0 0 0 1 0 0 0 - Write Users  8
-0 0 0 1 0 0 0 0 - Read Users 16
-
-0 0 1 0 0 0 0 0 - Write Settings 32
-0 1 0 0 0 0 0 0 - Read Settings 64
-
-//person 
-user = 1;
-guest = 1 | 4;	
-moderator =  1 | 2 | 4 | 16;
-admin = 1 | 2 | 4 | 8 | 16 | 32 | 64;	
+//Permissions
+0 0 0 0 0 0 1 - Access system
+0 0 0 0 0 1 0 - Write logs
+0 0 0 0 1 0 0 - Read logs
+0 0 0 1 0 0 0 - Write Users
+0 0 1 0 0 0 0 - Read Users
+0 1 0 0 0 0 0 - Write Settings
+1 0 0 0 0 0 0 - Read Settings
 
 */
 
